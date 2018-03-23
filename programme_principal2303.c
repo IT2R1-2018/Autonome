a3e5a2983646d1caf4ccec62e111f907659d94ca
JulienPally <36691918+JulienPally@users.noreply.github.com>
2018-03-23 12:34:40 +0100

    Add files via upload

---
+++ programme_principal2303.c
@@ -0,0 +1,264 @@
+#include "LPC17xx.h"
+#include <stdio.h>
+#include "GLCD.h"
+#include "GPIO.h"
+#include "cmsis_os.h"                       // CMSIS RTOS header file
+#include "Driver_SPI.h"
+#include "Driver_USART.h"               // ::CMSIS Driver:USART
+
+	extern ARM_DRIVER_USART Driver_USART2;
+
+	void Init_UART();
+	void PWM_Init();
+	void PWM_Droite(int rapport_cyclique);
+	void PWM_Gauche(int rapport_cyclique);
+	void LidarInit();
+	void affichage(int a);
+
+	void GetBTString(char *ptab,char delimFin);
+	
+	void automatique (void const *argument);                             // thread function
+	void gant (void const *argument);                             
+	void joystick (void const *argument);                             
+	void reception (void const *argument); 
+	
+	osThreadId tid_automatique, tid_gant, tid_joystick, tid_reception;                                          // thread id
+	
+	osThreadDef (automatique, osPriorityNormal, 1, 0);                   // thread object	
+	osThreadDef (gant, osPriorityNormal, 1, 0);                   
+	osThreadDef (joystick, osPriorityNormal, 1, 0);                   	
+	osThreadDef (reception, osPriorityNormal, 1, 0); 
+	
+	char tab[1], mode = 0;
+	
+int main (void) 
+{		
+		char  qualite,  angle1,  angle2,  distance1 ,  distance2, trame[5], a, RxBt;// Chaine de caractères (53 max en petite police / 20 max en grande police)
+		int somme, etat=0, k;
+		long int i;
+		short angle, distance;
+		
+		osKernelInitialize ();  
+		
+		Init_UART();
+	  Initialise_GPIO(); // init GPIO
+		PWM_Init();
+		//LidarInit();
+		GLCD_Init();						// Initialisation LCD
+		GLCD_Clear(White);			// Fond d'ecran en blanc
+		
+		LPC_GPIO2->FIODIR1=0x01;
+		
+		//LPC_UART2->THR = 0xA5; // Envoi de la donnée (valeur 0xA5)
+			
+		//LPC_UART2->THR = 0x20; // Envoi de la donnée (valeur 0x20)
+		
+		LPC_GPIO2->FIODIR0|=0x04;
+		
+		//GLCD_DisplayString(1,0,0,"Recherche de connexion...");
+		
+		Driver_USART2.Receive(tab,1);
+		while(Driver_USART2.GetRxCount()<1);
+		
+		//GLCD_DisplayString(1,0,0,"Connexion établie                ");
+		
+		if(tab[0] == '0') 
+		{
+			mode = 0; //mode auto
+			GLCD_DisplayString(1,0,0,"***Mode Automatique***                           ");
+			tid_automatique = osThreadCreate (osThread(automatique), NULL);
+		}
+		else if(tab[0] == '1') 
+		{
+			mode = 1; //mode gant
+			GLCD_DisplayString(1,0,0,"***Mode Force***                           ");
+			tid_gant = osThreadCreate (osThread(gant), NULL);
+		}
+		else if(tab[0] == '2')
+		{
+			mode = 2; //mode nunchuck
+			GLCD_DisplayString(1,0,0,"***Mode Wii***                           ");
+			tid_joystick = osThreadCreate (osThread(joystick), NULL);
+		}
+		
+		tid_reception = osThreadCreate (osThread(reception), NULL);
+		
+		osKernelStart ();                         // start thread execution 
+		osDelay(osWaitForever);
+}
+
+void automatique (void const *argument)
+{
+		int etat = 0;
+	
+		while(1)
+		{
+//			UART_PutChar(0xA5);
+			
+			switch (etat)
+		{
+         case 0: 
+					 PWM_Droite(70);
+					 PWM_Gauche(70);
+					 affichage(0);
+					 osDelay(1600);
+					 etat = 1;
+					 
+					 break;
+				 
+				 case 1: 
+					 PWM_Droite(70);
+					 PWM_Gauche(50);
+				   affichage(1);
+					 osDelay(700);
+				   etat = 2;
+					 
+					 break;
+				 
+				 case 2: 
+					 PWM_Droite(65);
+					 PWM_Gauche(65);
+				   affichage(2);
+					 osDelay(3000);
+				   etat = 3;
+					 
+				   break;
+					 
+				case 3: 
+					 PWM_Droite(70);
+					 PWM_Gauche(50);
+				   affichage(3);
+					 osDelay(600);
+				   etat = 0;
+				 
+					 break;	 
+    }
+		}
+}
+void gant (void const *argument)
+{
+	osDelay(osWaitForever);
+}
+void joystick (void const *argument)
+{
+	osDelay(osWaitForever);
+}
+void reception (void const *argument)
+{
+	while(1)
+	{
+	Driver_USART2.Receive(tab,1);
+	while(Driver_USART2.GetRxCount()<1);
+	
+	if(tab[0] == '0') 
+		{
+			mode = 0; //mode auto
+			GLCD_DisplayString(1,0,0,"***Mode Automatique***                           ");
+		}
+		else if(tab[0] == '1') 
+		{
+			mode = 1; //mode gant
+			GLCD_DisplayString(1,0,0,"***Mode Force***                           ");
+		}
+		else if(tab[0] == '2')
+		{
+			mode = 2; //mode nunchuck
+			GLCD_DisplayString(1,0,0,"***Mode Wii***                           ");
+		}
+	}
+}
+		/*while(1)
+		{	
+			
+			
+			a=UART_GetChar();
+			LPC_GPIO2->FIOPIN0|=0x04;
+			
+		if(a == 0xA5)
+		{	
+			
+			a=UART_GetChar();
+			if(a == 0x5A)
+			{
+				for(k=0; k<5 ; k++)
+				{
+					trame[k] = UART_GetChar();
+					
+				}
+			}
+		}
+		
+			if(trame[0]!=0x00)
+			{
+				qualite=(trame[0] >> 2);
+				angle1=(trame[1] >> 1);
+				angle2=trame[2];
+				distance1=trame[3];
+				distance2=trame[4];
+			
+				angle=((angle2<<7) | angle1) / 64.00;
+				distance=((distance2<<8) | distance1) /4.00;
+			}
+		
+		}*/
+	
+void PWM_Init()
+{
+	  LPC_PINCON->PINSEL4 |= 0x00000005;
+		LPC_SC->PCONP = LPC_SC->PCONP | 0x00000040;	// enable PWM1
+		
+		LPC_PWM1->PR = 11;  // prescaler
+		LPC_PWM1->MR0 = 99;
+		
+	  LPC_PWM1->MCR = LPC_PWM1->MCR | 0x00000002; // Timer relancé quand MR0 repasse à 0
+		LPC_PWM1->LER = LPC_PWM1->LER | 0x0000000F;  // ceci donne le droit de modifier dynamiquement la valeur du rapport cyclique
+	                                             // bit 0 = MR0    bit3 = MR3
+		LPC_PWM1->PCR = LPC_PWM1->PCR | (7<<9);  // autorise les sorties PWM1.1 et PWM1.2  
+		
+		LPC_PWM1->MR1 = 0; //remise à 0 des compteurs
+		LPC_PWM1->MR2 = 0;
+		LPC_PWM1->MR3 = 39;
+		
+		LPC_PWM1->TCR = 1;  /*validation de timer 1 et reset counter */
+}
+
+
+
+
+void PWM_Droite(int rapport_cyclique)
+{
+		LPC_PWM1->MR1 = rapport_cyclique;    // ceci ajuste la duree de l'état haut pour le moteur droit
+}
+
+void PWM_Gauche(int rapport_cyclique)
+{
+		LPC_PWM1->MR2 = rapport_cyclique;    // ceci ajuste la duree de l'état haut pour le moteur gauche
+}
+
+void LidarInit(int rapport_cyclique)
+{
+		LPC_PINCON->PINSEL7 = LPC_PINCON->PINSEL7 | 0x00300000;
+		LPC_PWM1->MR3 = rapport_cyclique;
+}
+
+void affichage(int a)
+{
+	char Chaine_char[53] = "";
+	
+	sprintf(Chaine_char, "Etat = %d", a);	// creation chaine texte
+	//GLCD_DisplayString(2,0,1,Chaine_char);		// affichage chaine ligne 0, colonne 0, petite police
+}
+
+void Init_UART()
+{
+	Driver_USART2.Initialize(NULL);
+	Driver_USART2.PowerControl(ARM_POWER_FULL);
+	Driver_USART2.Control(	ARM_USART_MODE_ASYNCHRONOUS |
+							ARM_USART_DATA_BITS_8		|
+							ARM_USART_STOP_BITS_1		|
+							ARM_USART_PARITY_NONE		|
+							ARM_USART_FLOW_CONTROL_NONE,
+							115200);
+	Driver_USART2.Control(ARM_USART_CONTROL_TX,1);
+	Driver_USART2.Control(ARM_USART_CONTROL_RX,1);
+}
